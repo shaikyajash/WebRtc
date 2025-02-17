63d06e027dcd986bdc1839ff7ef8f2734bcfc633
shaikyajash <shaikyajash@gmail.com>
2025-02-17 08:12:43 +0530

    ...

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
   cors:{
-    origin:['https://web-rtc-cyan-theta.vercel.app/'],
+    origin:'https://web-rtc-cyan-theta.vercel.app',
+    methods:['GET','POST']
   }
 });
 
