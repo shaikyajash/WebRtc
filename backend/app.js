5e7a28071d3e1b8b9e794fe4c05bc2d94e19e5a5
shaikyajash <shaikyajash@gmail.com>
2025-02-23 12:49:57 +0530

    bug fixed

--- backend/app.js
+++ backend/app.js
@@ -36,8 +36,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    socket.to(roomId).emit("user-disconnected");
-
     console.log("a client disconnected", socket.id);
   });
 });
